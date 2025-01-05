b8549d92cc10a7fe9d1749588add83f60094af93
sskings <sergiosantanadosreis@hotmail.com>
2025-01-05 16:58:32 -0300

    primeiro desafio do projeto: 1) adicionar novo elemento HTML emApp.jsx 2) criar estilo css em App.css. 3) alterar title do index.html.

---
+++ React_do_Zero_a_Maestria/intro/introducao/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p className="nova-regra">Uma nova tag foi inserida</p>
+    </>
+  );
+}
+
+export default App;
