d8fbdb70fb03f25a641a80c46db3afab8946e99b
sskings <sergiosantanadosreis@hotmail.com>
2025-01-07 20:03:33 -0300

    Renderização condicional; uso de if ternário;

--- React_do_Zero_a_Maestria/avancando_no_react/avancando/src/components/ConditionalRender.jsx
+++ React_do_Zero_a_Maestria/avancando_no_react/avancando/src/components/ConditionalRender.jsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 
 const ConditionalRender = () => {
   const [x] = useState(true);
+  const [nome, setNome] = useState("Fulano");
+
   return (
     <div>
-      <h1>Isso será exibido?</h1>
+      <h2>Isso será exibido?</h2>
       {x && <p>Se x for True, Sim!</p>}
       {!x && <p>Se x for False, Sim!</p>}
+
+      <h2>if ternário</h2>
+      {nome === "Cicrano" ? (
+        <div>
+          <p>O nome é Cicrano</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setNome("Cicrano")}>Clique aqui!</button>
     </div>
   );
 };
