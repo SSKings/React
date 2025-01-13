37700f7d82af3618e30ded5b0e99b1f6bf4c19fa
sskings <sergiosantanadosreis@hotmail.com>
2025-01-13 11:49:10 -0300

    css de componentes;  css inline; css inline dinâmico; classes dinâmicas; css modules;

--- React_do_Zero_a_Maestria/css_react/css/src/App.jsx
+++ React_do_Zero_a_Maestria/css_react/css/src/App.jsx
@@ -1,9 +1,48 @@
 import "./App.css";
-
+import MyComponent from "./component/MyComponent";
+import { useState } from "react";
+import Title from "./component/Title";
 function App() {
+  const n = 15;
+  const [nome] = useState("Sérgio");
+
+  const redTitle = true;
+
   return (
     <div>
+      {/* css global */}
       <h1>React com CSS</h1>
+      {/* css de component */}
+      <MyComponent />
+      {/* css do component vazando para App.js */}
+      <p>Este paragrafo é do App.js</p>
+      {/* inline css */}
+      <p
+        style={{
+          color: "blue",
+          padding: "25px",
+          borderTop: "2px solid red",
+          backgroundColor: "green",
+        }}
+      >
+        Este elemento foi estilizado de forma inline
+      </p>
+      {/* css inline dinâmico */}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "red" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={nome === "Sérgio" ? { backgroundColor: "cyan" } : null}>
+        Nome
+      </h2>
+      {/* classes dinâmicas */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título possui classe dinâmica
+      </h2>
+      {/* css modules */}
+      <Title />
     </div>
   );
 }
---
+++ React_do_Zero_a_Maestria/css_react/css/src/component/MyComponent.jsx
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Css De Component</h1>
+      <p>Este paragrafo é do component</p>
+      <p className="my-component-p">Este paragrafo tbm é do component</p>
+    </div>
+  );
+};
+
+export default MyComponent;
