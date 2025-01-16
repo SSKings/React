003f212d83fb5791bd12a58d03bf85c6d5e8dccf
sskings <sergiosantanadosreis@hotmail.com>
2025-01-16 15:33:06 -0300

    criando os componentes do game

--- React_do_Zero_a_Maestria/projeto_secret_word/secretword/src/App.jsx
+++ React_do_Zero_a_Maestria/projeto_secret_word/secretword/src/App.jsx
@@ -1,10 +1,29 @@
+//css
 import "./App.css";
+//react
+import { useCallBack, useEffect, useState } from "react";
+//data
+import { wordslist } from "../data/words";
+//components
 import StarScreen from "./components/StarScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 const App = () => {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordslist);
+
   return (
     <div className="App">
-      <StarScreen />
+      {gameStage === "start" && <StarScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 };
