6ffab6c25449972e4ae10bb2db8456c383c80756
sskings <sergiosantanadosreis@hotmail.com>
2025-01-17 15:44:58 -0300

    CSS da tela game; consumindo os states do game; Evento de inserção de letra

--- React_do_Zero_a_Maestria/projeto_secret_word/secretword/src/App.jsx
+++ React_do_Zero_a_Maestria/projeto_secret_word/secretword/src/App.jsx
@@ -23,6 +23,11 @@ const App = () => {
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
   const pickWordAndCategory = () => {
     //pick a random category
     const categories = Object.keys(words);
@@ -60,8 +65,8 @@ const App = () => {
   };
 
   // processa letras
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   };
   // restarts the game
   const retry = () => {
@@ -71,7 +76,18 @@ const App = () => {
   return (
     <div className="App">
       {gameStage === "start" && <StarScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "game" && (
+        <Game
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />
+      )}
       {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
