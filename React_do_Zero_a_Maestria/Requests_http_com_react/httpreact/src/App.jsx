4198848f1db1bbaa059ef860e84f71cc4955f527
sskings <sergiosantanadosreis@hotmail.com>
2025-01-21 11:01:21 -0300

    Seção de requisições http com react; Utilizando json server; Importância do useEffect; Resgatando dados da API; Adicionando dados (POST); Carregamento de dados dinâmico;

---
+++ React_do_Zero_a_Maestria/Requests_http_com_react/httpreact/src/App.jsx
@@ -0,0 +1,87 @@
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  // 2 - adição de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name: name,
+      price: price,
+    };
+
+    const res = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    // 3 - carregamento dinâmico
+    const addedProduct = await res.json();
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    setName("");
+    setPrice("");
+  };
+
+  return (
+    <div>
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.name} - R$: {p.price}
+          </li>
+        ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
