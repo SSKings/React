fd828d65416a27f1665ceb31d82f8c890c27b1ab
sskings <sergiosantanadosreis@hotmail.com>
2025-01-24 16:33:19 -0300

    Introdução a seção React Router; Configurando React Router; Navegando entre páginas; Carregando dados

---
+++ React_do_Zero_a_Maestria/react_router/react_router/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+// 1 - config react router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+// components
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        {/*2 - Links com react router*/}
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
