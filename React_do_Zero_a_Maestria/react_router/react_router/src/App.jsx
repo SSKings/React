806f93cd2710d46e391c311b4ba8ef9502b29a74
sskings <sergiosantanadosreis@hotmail.com>
2025-01-27 16:42:06 -0300

    Search Params; Redirecionamento de URL; Conclusão da seção

--- React_do_Zero_a_Maestria/react_router/react_router/src/App.jsx
+++ React_do_Zero_a_Maestria/react_router/react_router/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 
 // 1 - config react router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
-// components
-import NavBar from "./components/NavBar";
 import Info from "./pages/Info";
 import NotFound from "./pages/NotFound";
+// components
+import NavBar from "./components/NavBar";
+import SearchForm from "./components/SearchForm";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -18,13 +20,19 @@ function App() {
       <BrowserRouter>
         {/* 2 - Links com react router*/}
         <NavBar />
+        {/* 9 - search */}
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           {/* 4 - rotas dinâmicas */}
-          <Route path="products/:id" element={<Product />} />
+          <Route path="/products/:id" element={<Product />} />
           {/* 6 - nested routes */}
-          <Route path="products/:id/info" element={<Info />} />
+          <Route path="/products/:id/info" element={<Info />} />
+          {/* 9 - search */}
+          <Route path="/search" element={<Search />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to={"/about"} />} />
           {/* 7 - no match routes */}
           <Route path="*" element={<NotFound />} />
         </Routes>
