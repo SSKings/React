93ab5f52015a5565d2baa149e0fbbb7697fb8ba0
sskings <sergiosantanadosreis@hotmail.com>
2025-01-31 11:20:59 -0300

    refatorando contexto para hook; criando um contexto mais complexo; alterando um contexto mais complexo; concluindo seção

--- React_do_Zero_a_Maestria/context_api/context_api/src/pages/Info.jsx
+++ React_do_Zero_a_Maestria/context_api/context_api/src/pages/Info.jsx
@@ -1,12 +1,25 @@
 import { useContext } from "react";
 import { CounterContext } from "../context/CounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const Info = () => {
   const { counter } = useContext(CounterContext);
+
+  // 5 - contexto mais complexo
+  const { color, dispatch } = useTitleColorContext();
+
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
+
   return (
     <div>
-      <h1>Informações</h1>
+      <h1 style={{ color: color }}>Informações</h1>
       <p>Valor do contadot: {counter}</p>
+      <div>
+        <button onClick={() => setTitleColor("RED")}>RED</button>
+        <button onClick={() => setTitleColor("BLUE")}>BLUE</button>
+      </div>
     </div>
   );
 };
--- React_do_Zero_a_Maestria/context_api/context_api/src/pages/Principal.jsx
+++ React_do_Zero_a_Maestria/context_api/context_api/src/pages/Principal.jsx
@@ -1,16 +1,37 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 
-import { CounterContext } from "../context/CounterContext";
+// import { CounterContext } from "../context/CounterContext";
+
+//  4 - refatorando  com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
 import ChangeCounter from "../components/ChangeCounter";
 
+//  5 - contexto mais complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
 const Principal = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
+  // 5 - contexto mais complexo
+  const { color, dispatch } = useTitleColorContext();
+
+  // 6 - alterando contexto complexo
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
+
   return (
     <div>
-      <h1>Principal</h1>
+      <h1 style={{ color: color }}>Principal</h1>
       <p>Valor do contador: {counter}</p>
       {/* 3 - alterando o contexto */}
       <ChangeCounter />
+      {/* 6 - alterando contexto complexo */}
+      <div>
+        <button onClick={() => setTitleColor("RED")}>RED</button>
+        <button onClick={() => setTitleColor("BLUE")}>BLUE</button>
+      </div>
     </div>
   );
 };
