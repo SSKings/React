f8ebfdcae0b88e96ee553ab222111871b33fd65d
sskings <sergiosantanadosreis@hotmail.com>
2025-02-02 11:43:58 -0300

    CSS; Formulário de cadastro; states de fomr de cadastro e + ;

--- React_do_Zero_a_Maestria/projeto_mini_blog/mini_blog/src/components/NavBar.jsx
+++ React_do_Zero_a_Maestria/projeto_mini_blog/mini_blog/src/components/NavBar.jsx
@@ -5,16 +5,42 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-      <NavLink to="/">
-        Mini<span>Blog</span>
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blog</span>
       </NavLink>
-      <ul>
+      <ul className={styles.links_list}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/about">About</NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/register"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Register
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            About
+          </NavLink>
         </li>
       </ul>
     </nav>
